bed696721e7ef3d3d164a64f7fc114a3d641c2f8
Cephalonyx <cephalonyx@gmail.com>
2019-03-18 01:24:16 -0400

    Updates

---
+++ precache-manifest.6ff3179e120283434d3e4e176b9360b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7a9c0a938af6a81d6f6",
+    "url": "/basic_calculator/static/js/runtime~main.ec0a0f64.js"
+  },
+  {
+    "revision": "e763c894fff7ed5be7ae",
+    "url": "/basic_calculator/static/js/main.e9351178.chunk.js"
+  },
+  {
+    "revision": "e2feb934f6d6fef6fe41",
+    "url": "/basic_calculator/static/js/2.0518e2b9.chunk.js"
+  },
+  {
+    "revision": "e763c894fff7ed5be7ae",
+    "url": "/basic_calculator/static/css/main.8d176a56.chunk.css"
+  },
+  {
+    "revision": "bf43185daafa9f85987b3b72213a7242",
+    "url": "/basic_calculator/index.html"
+  }
+];
